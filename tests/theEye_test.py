3b0107ab9efb1eb111e808d25da80d6371c388b5
Bjorn Blomberg <brasse.bb@gmail.com>
2024-08-30 14:35:57 +0200

    adding reading of training data from file and updated test

--- tests/theEye_test.py
+++ tests/theEye_test.py
@@ -5,11 +5,13 @@ import os
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../theEye')))
 
 from theEye import get_training_data
+from theEye import DataPoint
 
 class TestTheEye(unittest.TestCase):
 
     def test_training_data(self):
-        self.assertEqual(get_training_data(), [1,0,1,0])
+        test_data = DataPoint(input_array = [1,1,1,1])
+        self.assertEqual(get_training_data()[0].input_array, test_data.input_array)
 
 if __name__ == '__main__':
     unittest.main()
--- theEye/theEye.py
+++ theEye/theEye.py
@@ -3,10 +3,34 @@ def main():
     #(1 = ON, 0 = OFF)
     input_data = [1, 0, 1, 1]
     print(input_data)
+    leaning_data = get_training_data("training_data.txt")
+    print(leaning_data)
 
-def get_training_data():
-    training_data = [1,0,1,0]
-    return training_data
+def get_training_data(file_path):
+    data_points = []
+    with open(file_path, 'r') as file:
+        for line in file:
+            parts = line.strip().split(' = ')
+            array_str = parts[0]
+            value_str = parts[1]
+
+            array = list(map(int, array_str.split(',')))
+
+            value = int(value_str)
+
+            data_point = DataPoint(array, value)
+            data_points.append(data_point)
+
+    return data_points
+
+class DataPoint:
+    def __init__(self, input_array = [1,1,1,1], output_value = 1):
+        self.input_array = input_array
+        self.output_value = output_value
+
+    def __repr__(self):
+        return f"(Input={self.input_array}, output={self.output_value})"
 
 if __name__ == "__main__":
     main()
+
